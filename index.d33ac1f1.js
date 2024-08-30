cc3bbfd2db0ec8e08d2fdb82e2408a84c3160086
sinner1993 <sergio1993goga@gmail.com>
2024-08-30 19:43:43 +0200

    make build

---
+++ index.d33ac1f1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__img");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");r&&(r.getAttribute("href"),e.setAttribute("src",r))});
+//# sourceMappingURL=index.d33ac1f1.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const gallery = document.querySelector(".gallery__img");
+const ul = document.querySelector(".gallery__list");
+// eslint-disable-next-line no-shadow
+ul.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const img = event.target.closest(".list-item__link");
+    if (!img) return;
+    img.getAttribute("href");
+    gallery.setAttribute("src", img);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
